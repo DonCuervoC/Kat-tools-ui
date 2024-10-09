bf71c082e138e6e68d7713a24fbb495f07f8c5f5
DonCuervoC <nelson.cuervo89@gmail.com>
2024-10-09 19:45:28 -0400

    Layout.tsx correction 05

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 // app/layout.tsx
-import { ReactNode } from 'react';
+// import { ReactNode } from 'react';
 import "./globals.css";
 import { rubik } from '@/ui/fonts';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Metadata } from 'next';
 
-interface LayoutProps {
-  children: ReactNode;
-  types?: any;
-}
+// interface LayoutProps {
+//   children: ReactNode;
+
+// }
 
 export const metadata: Metadata = {
   title: "Office tools",
@@ -17,7 +17,18 @@ export const metadata: Metadata = {
 
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-unused-vars
-export default function RootLayout({ children, types }: LayoutProps) {
+// export default function RootLayout({ children }: LayoutProps) {
+//   return (
+//     <html lang="en">
+//       <body className={`${rubik.className} antialiased`}>
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+
+export default function RootLayout({ children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
       <body className={`${rubik.className} antialiased`}>
