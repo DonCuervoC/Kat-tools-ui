69078fefd3ea0bd3727a76e2d775a8ccc554a0f7
DonCuervoC <nelson.cuervo89@gmail.com>
2024-10-13 21:16:57 -0400

    welcome OK

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,28 +2,41 @@
 
 import Sidebar from './Sidebar';
 import Content from './Content';
-import BuildingPage from '../components/BuildingPage';
+// import BuildingPage from '../components/BuildingPage';
 import TvContainer from '../components/tveffect/TvContainer';
 // import TvEffect from '../components/tveffect/TvEffect';
+import Image from 'next/image';
+import { Spinner } from 'react-bootstrap';
 
 
 export default function DashboardPage() {
   return (
     <div style={{ display: 'flex', flex: '1' }}> {/* Asegura que el sidebar y el contenido usen el espacio disponible */}
-    '
-    <TvContainer>
-    <Sidebar />
-      <Content>
+      '
+      <TvContainer>
+        <Sidebar />
+        <Content>
 
-    
+          <div style={{ display: 'block', marginLeft: '100px', marginRight: 'auto', marginTop: '100px' }}>
+            {/* <TvEffect timeMs={800}> */}
+            {/* <BuildingPage></BuildingPage> */}
+            {/* </TvEffect> */}
+            <Spinner animation="grow"
+              style={{ marginLeft: '90px', marginRight: 'auto', marginTop: '100px', width: '40px', height: '40px' }}
+              />
+            <Image
+              width={130}
+              height={170}
+              src="/cats/cat33.jpg"
+              alt="Cat contact"
+              style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+            />
+            <br></br>
+            <p><strong>Welcome</strong> and <strong>enjoy</strong> the tools!</p>
 
-        <div>
-          {/* <TvEffect timeMs={800}> */}
-          <BuildingPage></BuildingPage>
-          {/* </TvEffect> */}
-        </div>
+          </div>
 
-      </Content>
+        </Content>
       </TvContainer>
     </div>
   );
