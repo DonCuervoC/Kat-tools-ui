f299d7fb6fa6b80d459daa6aebde95cc830640d3
DonCuervoC <nelson.cuervo89@gmail.com>
2024-10-13 20:13:12 -0400

    query register OK

--- app/api/visitors/route.ts
+++ app/api/visitors/route.ts
@@ -9,11 +9,11 @@ const pool = new Pool({
 
 //Definir el tipo de datos del formulario
 type VisitorFormData = {
-    companyName: string;
-    name: string;
-    email: string;
-    type: 'anonymous' | 'registered';
-    date: string;
+  companyName: string;
+  name: string;
+  email: string;
+  type: 'anonymous' | 'registered';
+  date: string;
 };
 
 // Función para registrar al visitante en la base de datos
@@ -21,35 +21,43 @@ const registerVisitor = async (data: VisitorFormData) => {
   const { companyName, name, email, type } = data;
 
   // Inserta los datos en la base de datos
-  const query = `
-    INSERT INTO visitors (company_name, name, email, type, visit_date)
-    VALUES ($1, $2, $3, $4, NOW())
-  `;
-  const values = [companyName, name, email, type];
+  /*
+const query = 
+`
+  INSERT INTO visitors (company_name, name, email, type, visit_date)
+  VALUES ($1, $2, $3, $4, NOW())
+`;
+const values = [companyName, name, email, type];
+*/
 
+  const query = `
+INSERT INTO visitors (company_name, name, email, type, visit_date, from_app)
+VALUES ($1, $2, $3, $4, NOW(), $5)
+`;
+  const values = [companyName, name, email, type, 'kat_tools'];
   await pool.query(query, values);
 };
 
 export const POST = async (req: Request) => {
-    try {
-        const data: VisitorFormData = await req.json(); // Obtiene los datos del cuerpo de la solicitud
-        // const now = new Date();
+  try {
+    const data: VisitorFormData = await req.json(); // Obtiene los datos del cuerpo de la solicitud
+    // const now = new Date();
 
-        if (data.type === 'anonymous') {
-            data.companyName = 'n/a'
-            data.name = 'n/a'
-            data.email = 'n/a'
-        }
-        // data.date = String(now);
-
-        console.log(data);
-
-        await registerVisitor(data);
-        return NextResponse.json({ message: 'Visitor registered successfully.' }, { status: 201 });
-    } catch (error) {
-        console.error('Error registering visitor:', error);
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    if (data.type === 'anonymous') {
+      data.companyName = 'n/a'
+      data.name = 'n/a'
+      data.email = 'n/a'
     }
+    // data.date = String(now);
+
+    console.log(data);
+
+    await registerVisitor(data);
+    return NextResponse.json({ message: 'Visitor registered successfully.' }, { status: 201 });
+  } catch (error) {
+    console.error('Error registering visitor:', error);
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
 };
 
 
