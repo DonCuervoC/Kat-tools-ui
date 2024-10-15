e7df8b06e558a7faeca1d9f088612a3cb6007dda
DonCuervoC <nelson.cuervo89@gmail.com>
2024-10-15 15:37:47 -0400

    format correction

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,31 +11,36 @@ import { Spinner } from 'react-bootstrap';
 
 export default function DashboardPage() {
   return (
-    <div style={{ display: 'flex', flex: '1' }}> {/* Asegura que el sidebar y el contenido usen el espacio disponible */}
-      '
+    <div style={{ display: 'flex', flex: '1', position: 'relative' }}> {/* Asegura que el sidebar y el contenido usen el espacio disponible */}
       <TvContainer>
         <Sidebar />
         <Content>
-
-          <div style={{ display: 'block', marginLeft: '100px', marginRight: 'auto', marginTop: '100px' }}>
-            {/* <TvEffect timeMs={800}> */}
-            {/* <BuildingPage></BuildingPage> */}
-            {/* </TvEffect> */}
-            <Spinner animation="grow"
-              style={{ marginLeft: '90px', marginRight: 'auto', marginTop: '100px', width: '40px', height: '40px' }}
-              />
+          <div style={{ 
+              display: 'flex', 
+              flexDirection: 'column', 
+              justifyContent: 'center', 
+              alignItems: 'center', 
+              height: '100vh', 
+              width: '100%', // Ocupa todo el ancho disponible
+              textAlign: 'center', // Centra el texto
+              position: 'absolute', // Centrado absoluto
+              left: '50%', // Desplaza el contenedor al centro de la pantalla
+              top: '50%',
+              transform: 'translate(-50%, -60%)' // Ajusta para centrarlo perfectamente
+          }}>
+            <Spinner 
+              animation="grow"
+              style={{ width: '40px', height: '40px', marginBottom: '20px' }}
+            />
             <Image
               width={130}
               height={170}
               src="/cats/cat33.jpg"
               alt="Cat contact"
-              style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }}
+              style={{ marginBottom: '20px' }}
             />
-            <br></br>
             <p><strong>Welcome</strong> and <strong>enjoy</strong> the tools!</p>
-
           </div>
-
         </Content>
       </TvContainer>
     </div>
